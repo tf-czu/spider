27477f4836297cf516fc45cc93e5b35b855a781f
Milan <milan.petrik@protonmail.com>
2024-10-17 22:18:38 +0200

    Add test_circular_trajectory to test_localization

--- lib/test_localization.py
+++ lib/test_localization.py
@@ -7,6 +7,7 @@
 import unittest
 import datetime
 import random
+import math
 import localization as lo
 
 #import teachmedijkstra
@@ -25,15 +26,59 @@ class TestLocalization(unittest.TestCase):
             velocity = [random.randint(-10, 10) for j in range(3)]
             num_steps = 10
             loc = lo.Localization(remember_history = True)
-            xyz = [0, 0, 0]
+            gps_xyz = [0, 0, 0]
             for k in range(num_steps):
-                loc.update_xyz_from_gps(time, xyz, gps_err = gps_err)
+                loc.update_xyz_from_gps(time, gps_xyz, gps_err = gps_err)
                 pose3d = loc.get_pose3d()
                 (filtered_x, filtered_y, __), __ = pose3d
                 # otestuj, zda `filtered_x` a `filtered_y` jsou ve stejnem pomeru,
                 # jako `velocity[0]` a `velocity[1]`
                 self.assertAlmostEqual(velocity[1]*filtered_x, velocity[0]*filtered_y, places = 7)
-                xyz = [xyz[i] + period*velocity[i] for i in range(len(xyz))]
+                gps_xyz = [gps_xyz[i] + period*velocity[i] for i in range(len(gps_xyz))]
+        #loc.draw()
+
+    def test_circular_trajectory(self):
+        # Simuluje rovnomerny pohyb po kruznici
+        time = datetime.timedelta(seconds = 0)
+        gps_err = [0.1, 0.1, 0.1]
+        freq = 10 # Hz
+        period = 1 / freq
+        angle = 0
+        angular_velocity = 1 # rad / s
+        num_steps = 30
+        loc = lo.Localization(remember_history = True)
+        for k in range(num_steps):
+            gps_xyz = [math.cos(angle), math.sin(angle), 0]
+            loc.update_xyz_from_gps(time, gps_xyz, gps_err = gps_err)
+            pose3d = loc.get_pose3d()
+            (filtered_x, filtered_y, __), __ = pose3d
+            self.assertAlmostEqual(gps_xyz[0], filtered_x, places = 1)
+            self.assertAlmostEqual(gps_xyz[1], filtered_y, places = 1)
+            angle += angular_velocity * period
+        #loc.draw()
+
+    #def test_circular_trajectory_with_extrapolation(self):
+    #    # Simuluje rovnomerny pohyb po kruznici
+    #    time = datetime.timedelta(seconds = 0)
+    #    gps_err = [0.1, 0.1, 0.1]
+    #    freq = 10 # Hz
+    #    period = 1 / freq
+    #    period_timedelta = datetime.timedelta(seconds = period)
+    #    angle = 0
+    #    angular_velocity = 1 # rad / s
+    #    num_steps = 50
+    #    gps_step = 10 # GPS bude aktualizovana v kazdem 5. kroku
+    #    loc = lo.Localization(remember_history = True)
+    #    for k in range(num_steps):
+    #        if k % gps_step == 0:
+    #            angle = angular_velocity * time.total_seconds()
+    #            print('angle:', angle)
+    #            gps_xyz = [math.cos(angle), math.sin(angle), 0]
+    #            loc.update_xyz_from_gps(time, gps_xyz, gps_err = gps_err)
+    #        pose3d = loc.get_pose3d(time)
+    #        (filtered_x, filtered_y, __), __ = pose3d
+    #        time += period_timedelta
+    #    loc.draw()
 
 if __name__ == '__main__':
     unittest.main()
