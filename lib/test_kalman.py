855acad15b5e05a8d90001c2c3a832cb430fba37
Milan <milan.petrik@protonmail.com>
2024-10-18 13:46:01 +0200

    Add test_uniform_linear_motion() to test_kalman.py

--- lib/test_kalman.py
+++ lib/test_kalman.py
@@ -32,5 +32,40 @@ class TestKalmanLocalization(unittest.TestCase):
         for i in range(3):
             self.assertAlmostEqual(last_xyz[i], 10.0, places = 0)
 
+    def test_uniform_linear_motion(self):
+        # simuluje rovnomerny primocary pohyb ve smeru vektoru `velocity`
+        # testuje, zda jsou po 20ti krocich filtrovane souradnice blizke (5%)
+        # vstupnim souradnicim
+        kf = kalman.KalmanFilterLocalization()
+        time = datetime.timedelta(seconds = 0)
+        xyz = [0, 0, 0]
+        xyz_err = [1, 1, 1]
+        kf.input(xyz, time.total_seconds(), xyz_err)
+        velocity = [1, 2, 0]
+        freq = 10 # Hz
+        period = 1 / freq
+        period_timedelta = datetime.timedelta(seconds = period)
+        for k in range(20):
+            time += period_timedelta
+            new_xyz = [xyz[i] + velocity[i]*period for i in range(3)]
+            xyz = new_xyz
+            kf.input(xyz, time.total_seconds(), xyz_err)
+            __, last_xyz = kf.get_last_xyz()
+            #print(xyz, last_xyz)
+        #print('---', xyz, last_xyz)
+        relative_error = 3 * [None]
+        for i in range(3):
+            num = abs(xyz[i] - last_xyz[i])
+            den = xyz[i]
+            if den == 0 and num == 0:
+                relative_error[i] = 0
+            elif den == 0:
+                relative_error[i] = 1
+            else:
+                relative_error[i] = num / den
+            self.assertLess(relative_error[i], 0.05) # aby relativni chyba byla mensi nez 5%
+
+
+
 
 
