c01e5e0d59bb882e8583ae9c9d74be9ad377f090
Milan <milan.petrik@protonmail.com>
2025-05-02 17:45:12 +0200

    Fix run.py

--- run.py
+++ run.py
@@ -9,36 +9,45 @@ from osgar.bus import Bus
 from lsqr_localization import LeastSquaresLocalization
 
 def main():
-    #arg_parser = argparse.ArgumentParser(description = 'Plots GPS and odometry data from OSGAR log. Fits the odometry trajectory to the GPS trajectory.')
-    arg_parser = argparse.ArgumentParser()
+    arg_parser = argparse.ArgumentParser(description = 'Applies LeastSquaresLocalization to an OSGAR log; plots the results')
     arg_parser.add_argument("input", help = "path to input log file")
-    #arg_parser.add_argument("-p", help = "post-process window size in meters")
-    #arg_parser.add_argument("-w", help = "window size in meters")
-    #arg_parser.add_argument("--prune", help = "pruning of the input data")
+    arg_parser.add_argument("-p", help = "post-process window size in meters")
+    arg_parser.add_argument("-w", help = "window size in meters")
+    arg_parser.add_argument("--prune", help = "pruning of the input data")
+    arg_parser.add_argument("--init-win", help = "initial window [m]")
+    arg_parser.add_argument("--init-sca", help = "initial scale")
+    arg_parser.add_argument("--init-ang", help = "initial angle [deg]")
     args = arg_parser.parse_args()
     path_log = args.input
 
-    #if args.p:
-    #    post_window_in_meters = int(args.p)
-    #else:
-    #    post_window_in_meters = None
-    #if args.w:
-    #    window_in_meters = int(args.w)
-    #else:
-    #    window_in_meters = None
-    #if args.prune:
-    #    prune = int(args.prune)
-    #else:
-    #    prune = 1
+    window = 5
+    post_window = 5
+    prune = 20
+    initial_scale = 1
+    initial_window = 1
+    initial_angle = -75
+
+    if args.w:
+        window = float(args.w)
+    if args.p:
+        post_window = float(args.p)
+    if args.prune:
+        prune = int(args.prune)
+    if args.init_win:
+        init_win = float(args.init_win)
+    if args.init_sca:
+        init_sca = float(args.init_sca)
+    if args.init_ang:
+        init_ang = float(args.init_ang)
 
     bus = Bus(MagicMock())
     config = {
-        'window': 5,
-        'post_window': 5,
-        'prune': 1,   
-        'initial_scale':  1.0,   
-        'initial_window': 1.0,   
-        'initial_angle': -75,   
+        'window':          window,
+        'post_window':     post_window,
+        'prune':           prune,
+        'initial_scale':   initial_scale,
+        'initial_window':  initial_window,
+        'initial_angle':   initial_angle,
     }
 
     id_ori = lookup_stream_id(path_log, "imu.orientation")
@@ -47,42 +56,26 @@ def main():
     id_enc = lookup_stream_id(path_log, "spider.encoders")
 
     list_of_stream_ids = [id_ori, id_gps, id_p2d, id_enc]
-    list_of_stream_ids = [id_ori, id_gps, id_p2d]
 
-    #localization = LeastSquaresLocalization(config, bus.handle('abc'))
+    localization = LeastSquaresLocalization(config, bus.handle('abc'))
 
-    with LogReader(path_log, list_of_stream_ids) as log:
+    with LogReader(path_log, only_stream_id = list_of_stream_ids) as log:
         counter = 0
         for timestamp, stream_id, data_raw in log:
-            #print("timestamp:", timestamp, type(timestamp))
-            #if stream_id == only_stream_odo:
-            #    localization.add_odo(timestamp, deserialize(data_raw))
-            #elif stream_id == only_stream_ori:
-            #    localization.add_ori(timestamp, deserialize(data_raw))
-            #elif stream_id == only_stream_gps:
-            #    localization.add_gps(timestamp, deserialize(data_raw))
-            #localization.time = timestamp
+            localization.time = timestamp
             if stream_id == id_ori:
-                #print("ORI", deserialize(data_raw))
-                #localization.on_orientation(deserialize(data_raw))
-                pass
+                localization.on_orientation(deserialize(data_raw))
             elif stream_id == id_gps:
-                #print("GPS", deserialize(data_raw))
-                #localization.on_nmea(deserialize(data_raw))
-                pass
+                localization.on_nmea(deserialize(data_raw))
             elif stream_id == id_p2d:
-                #print("P2D", deserialize(data_raw))
-                #localization.on_pose2d(deserialize(data_raw))
-                pass
-            #elif stream_id == id_enc:
-            #    #print("ENC", deserialize(data_raw))
-            #    #localization.on_encoders(deserialize(data_raw))
-            #    pass
+                localization.on_pose2d(deserialize(data_raw))
+            elif stream_id == id_enc:
+                localization.on_encoders(deserialize(data_raw))
             counter += 1
             if counter % 1000 == 0:
                 print(counter)
 
-    #localization.draw()
+    localization.draw()
 
 if __name__ == "__main__":
     main()
