627d1adae938a822c8320c4d9f96663ebe561577
Milan <milan.petrik@protonmail.com>
2025-05-06 14:21:23 +0200

    Add lib/test_tracker_lsqr.py

--- lib/test_tracker.py
+++ lib/test_tracker.py
@@ -1,9 +1,5 @@
 # -*- coding: UTF-8 -*-
 
-"""
-    Implementation of unit tests for least_squares_localization
-"""
-
 import unittest
 from unittest.mock import MagicMock
 
@@ -14,7 +10,6 @@ class TestTracker(unittest.TestCase):
     def test_abstract_class(self):
         with self.assertRaises(TypeError):
             t = tracker.Tracker()
-
 
 if __name__ == '__main__':
     unittest.main()
