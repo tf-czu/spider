b77d397e4f513522ff0e582bfc6e49a4dd25d914
Milan <milan.petrik@protonmail.com>
2025-06-02 15:17:06 +0200

    Add test_interpolate_xyz_in_time__error_run()

--- lib/test_rmsd.py
+++ lib/test_rmsd.py
@@ -8,7 +8,7 @@ from rmsd import *
 
 class TestRootMeanSquareDeviationCounter(unittest.TestCase):
 
-    def test_interpolate_xyz_in_time(self):
+    def test_interpolate_xyz_in_time__normal_run(self):
         xyz_1 = [2, 1, 0]
         xyz_2 = [3, 6, 7]
         for k in range(11):
@@ -17,6 +17,14 @@ class TestRootMeanSquareDeviationCounter(unittest.TestCase):
             expected_xyz = [(1 - alpha)*xyz_1[i] + alpha*xyz_2[i] for i in range(len(xyz_1))]
             for i in range(len(xyz_1)):
                 self.assertAlmostEqual(xyz[i], expected_xyz[i])
+
+    def test_interpolate_xyz_in_time__error_run(self):
+        xyz_1 = [2, 1, 0]
+        xyz_2 = [3, 6, 7]
+        with self.assertRaises(AssertionError):
+            interpolate_xyz_in_time(timedelta(seconds = 6), xyz_1, timedelta(seconds = 5), xyz_2, timedelta(seconds = 5.5))
+        with self.assertRaises(AssertionError):
+            interpolate_xyz_in_time(timedelta(seconds = 5), xyz_1, timedelta(seconds = 6), xyz_2, timedelta(seconds = 6.5))
 
     def test_1(self):
         rmsd_counter = RootMeanSquareDeviationCounter()
