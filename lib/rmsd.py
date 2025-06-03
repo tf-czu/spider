c3ac2560014f52e85eb021774784ea76a2badc65
Milan <milan.petrik@protonmail.com>
2025-06-03 09:11:12 +0200

    Remove interpolate_xyz_in_time()

--- lib/rmsd.py
+++ lib/rmsd.py
@@ -2,32 +2,6 @@ import math
 import copy
 import bisect
 
-def interpolate_xyz_in_time(time_1, xyz_1, time_2, xyz_2, time):
-    """
-        Computes interpolation of two time-stamped positions in (3D) space.
-
-        Args:
-            time_1 (datetime.timedelta): time of the first position
-            xyz_1 (list of float): first position as a list of (three)
-                coordinates
-            time_2 (datetime.timedelta): time of the second position
-            xyz_2 (list of float): second position as a list of (three)
-                coordinates
-            time (datetime.timedelta): time between `time_1` and `time_2` where
-                the interpolation is to be computed
-
-        Returns (list of float): interpolated position as a list of (three)
-            coordinates
-    """
-    assert time_1 <= time <= time_2
-    if time_1 == time == time_2:
-        return copy.copy(xyz_1)
-    t_1 = time_1.total_seconds()
-    t_2 = time_2.total_seconds()
-    t = time.total_seconds()
-    alpha = (t - t_1) / (t_2 - t_1)
-    return [(1 - alpha)*xyz_1[i] + alpha*xyz_2[i] for i in range(len(xyz_1))]
-
 class RootMeanSquareDeviationCounter:
     """
         Coputes root mean square deviation from two asynchronously provided
