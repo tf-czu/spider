b26d078cf34dc07bc8add4c6c5282928a9e4945b
Milan <milan.petrik@protonmail.com>
2025-06-03 10:28:58 +0200

    Add dimensions 2 and 3 to lib/test_rmsd.py

--- lib/test_rmsd.py
+++ lib/test_rmsd.py
@@ -14,6 +14,8 @@ class TestRootMeanSquareDeviationCounter(unittest.TestCase):
             rmsd_counter.add_A(timedelta(seconds = t), [t, t, 0])
             rmsd_counter.add_B(timedelta(seconds = t), [t+2, t, 0])
         self.assertAlmostEqual(rmsd_counter.compute_rmsd(), 2)
+        self.assertAlmostEqual(rmsd_counter.compute_rmsd(2), 2)
+        self.assertAlmostEqual(rmsd_counter.compute_rmsd(3), 2)
 
     def test_compute_rmsd__parallel_lines__with_interpolation(self):
         rmsd_counter = RootMeanSquareDeviationCounter()
@@ -23,6 +25,8 @@ class TestRootMeanSquareDeviationCounter(unittest.TestCase):
             else:
                 rmsd_counter.add_B(timedelta(seconds = t), [t+2, t, 0])
         self.assertAlmostEqual(rmsd_counter.compute_rmsd(), 2)
+        self.assertAlmostEqual(rmsd_counter.compute_rmsd(2), 2)
+        self.assertAlmostEqual(rmsd_counter.compute_rmsd(3), 2)
 
     def test_compute_rmsd__non_parallel_lines__without_interpolation(self):
         rmsd_counter = RootMeanSquareDeviationCounter()
@@ -31,6 +35,8 @@ class TestRootMeanSquareDeviationCounter(unittest.TestCase):
             rmsd_counter.add_B(timedelta(seconds = t), [t, 0, 0])
         expected_rmsd = math.sqrt(sum([i**2 for i in range(10)]) / 10)
         self.assertAlmostEqual(rmsd_counter.compute_rmsd(), expected_rmsd)
+        self.assertAlmostEqual(rmsd_counter.compute_rmsd(2), expected_rmsd)
+        self.assertAlmostEqual(rmsd_counter.compute_rmsd(3), expected_rmsd)
 
     def test_compute_rmsd__non_parallel_lines__with_interpolation(self):
         rmsd_counter = RootMeanSquareDeviationCounter()
@@ -41,6 +47,8 @@ class TestRootMeanSquareDeviationCounter(unittest.TestCase):
                 rmsd_counter.add_B(timedelta(seconds = t), [t, 0, 0])
         expected_rmsd = math.sqrt(sum([i**2 for i in range(2, 10, 2)]) / 4)
         self.assertAlmostEqual(rmsd_counter.compute_rmsd(), expected_rmsd)
+        self.assertAlmostEqual(rmsd_counter.compute_rmsd(2), expected_rmsd)
+        self.assertAlmostEqual(rmsd_counter.compute_rmsd(3), expected_rmsd)
 
     def test_compute_rmsd__two_coords__without_interpolation(self):
         rmsd_counter = RootMeanSquareDeviationCounter()
@@ -48,6 +56,8 @@ class TestRootMeanSquareDeviationCounter(unittest.TestCase):
             rmsd_counter.add_A(timedelta(seconds = t), [t, t, 0])
             rmsd_counter.add_B(timedelta(seconds = t), [t+1, t+1, 0])
         self.assertAlmostEqual(rmsd_counter.compute_rmsd(), math.sqrt(2))
+        self.assertAlmostEqual(rmsd_counter.compute_rmsd(2), math.sqrt(2))
+        self.assertAlmostEqual(rmsd_counter.compute_rmsd(3), math.sqrt(2))
 
     def test_compute_rmsd__two_coords__with_interpolation(self):
         rmsd_counter = RootMeanSquareDeviationCounter()
@@ -57,4 +67,6 @@ class TestRootMeanSquareDeviationCounter(unittest.TestCase):
             else:
                 rmsd_counter.add_B(timedelta(seconds = t), [t+1, t+1, 0])
         self.assertAlmostEqual(rmsd_counter.compute_rmsd(), math.sqrt(2))
+        self.assertAlmostEqual(rmsd_counter.compute_rmsd(2), math.sqrt(2))
+        self.assertAlmostEqual(rmsd_counter.compute_rmsd(3), math.sqrt(2))
 
