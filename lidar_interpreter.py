bade83ce48d3047640e695bed0c99e4d63d9bd5b
Milan <milan.petrik@protonmail.com>
2026-05-25 14:55:23 +0200

    Add lidar_interpreter.py

---
+++ lidar_interpreter.py
@@ -0,0 +1,32 @@
+# -*- coding: UTF-8 -*-
+
+import numpy as np
+
+from osgar.node import Node
+
+class LidarInterpreter(Node):
+    """
+        Reads Lidar data and returns: obstacle map, slope map.
+    """
+
+    def __init__(self, config, bus):
+        super().__init__(config, bus)
+
+        # register a stream to be published
+        #bus.register('pose3d')
+
+        # for debugging
+        self.verbose = False # super-class Node sets this to `True` if --verbose parameter is applied
+
+    def on_lidar_scan3d(self, data):
+        """
+            Process one Lidar scan.
+
+            Args:
+                data (list of list of int): array of size 32 x 1024
+        """
+        print(np.array(data).shape)
+
+    def draw(self):
+        pass
+
