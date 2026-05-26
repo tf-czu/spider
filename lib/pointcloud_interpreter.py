3bec49082f5e056ffe592cdfff8129f0cc9d73f5
Milan <milan.petrik@protonmail.com>
2026-05-26 18:19:11 +0200

    Revise identifiers in pointcloud_interpreter.py

--- lib/pointcloud_interpreter.py
+++ lib/pointcloud_interpreter.py
@@ -5,8 +5,8 @@ import ouster.sdk as ous
 
 class PointCloudInterpreter:
     """
-        Reads point-cloud scans and returns:
-            traversability map, obstacle map, slope map.
+        Processes accumulated point clouds and returns maps:
+            traversability map, obstacle map, slope map, ...
     """
 
     def __init__(self, output_frequency = 1.0):
@@ -15,19 +15,19 @@ class PointCloudInterpreter:
         self.last_output_time = None # [datetime.timedelta]
         self.remembered_outputs = 0
         self.fifo_of_outputs = []
-        self.list_of_accumulated_scans = []
+        self.list_of_accumulated_point_clouds = []
 
-    def update(self, timestamp, scan):
-        self.list_of_accumulated_scans.append(scan)
+    def update(self, timestamp, points):
+        self.list_of_accumulated_point_clouds.append(points)
         if self.last_output_time is None:
             self.last_output_time = timestamp
             return False
         elif (timestamp - self.last_output_time).total_seconds() >= self.output_period:
-            self.process_accumulated_scans(timestamp)
+            self.process_accumulated_point_clouds(timestamp)
             self.last_output_time = timestamp
             return True
         else:
             return False
 
-    def process_accumulated_scans(self, timestamp):
+    def process_accumulated_point_clouds(self, timestamp):
         print("PROCESSING")
