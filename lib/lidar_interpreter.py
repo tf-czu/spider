d16767f18c56b9bc046c42fcece5b58ad3975b73
Milan <milan.petrik@protonmail.com>
2026-05-26 16:32:47 +0200

    Change last_output_time: [s] -> datetime.timedelta

--- lib/lidar_interpreter.py
+++ lib/lidar_interpreter.py
@@ -10,22 +10,22 @@ class LidarInterpreter:
     def __init__(self, output_frequency = 1.0):
         self.output_frequency = output_frequency # [Hz]
         self.output_period = 1.0 / output_frequency # [s]
-        self.last_output_time = None # [s]
+        self.last_output_time = None # [datetime.timedelta]
         self.remembered_outputs = 0
+        self.fifo_of_outputs = []
         self.list_of_accumulated_scans = []
 
     def update(self, timestamp, scan):
         self.list_of_accumulated_scans.append(scan)
-        t = timestamp.total_seconds()
         if self.last_output_time is None:
-            self.last_output_time = t
+            self.last_output_time = timestamp
             return False
-        elif t - self.last_output_time >= self.output_period:
+        elif (timestamp - self.last_output_time).total_seconds() >= self.output_period:
             self.process_accumulated_scans(timestamp)
-            self.last_output_time = t
+            self.last_output_time = timestamp
             return True
         else:
             return False
 
     def process_accumulated_scans(self, timestamp):
-        print("PROCESSED")
+        print("PROCESSING")
